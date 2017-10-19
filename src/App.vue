2bec53c1fb1c44b12c7cc86546ac6c60f6c55e3b
prudywsh <antoineprudhomme5@gmail.com>
2017-10-19 09:23:11 +0200

    default install cleaned

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Workflow />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Workflow from './components/Workflow'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Workflow
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Workflow.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Connexion au réseau UTT arena</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Workflow'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
