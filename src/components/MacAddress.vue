1bc2e56da076427cb475ca30fe5e33f3580810d5
prudywsh <antoineprudhomme5@gmail.com>
2017-10-20 15:00:08 +0200

    display help according to the user's OS

--- src/components/MacAddress.vue
+++ src/components/MacAddress.vue
@@ -4,13 +4,35 @@
     <div class="center-form-container">
       <form class="center-form">
         <div class="input-group">
-          <input v-model="macAddress" class="a-input" type="text" placeholder="Adresse MAC" />
+          <input v-model="macAddress" class="a-input" type="text" placeholder="xx:xx:xx:xx:xx:xx" />
           <div class="form-error" v-if="submitted && !validateMacAddress">Adresse MAC invalide</div>
         </div>
         <div class="center">
           <button type="button" class="a-button a-button--blue" @click="sendMacAddress">envoyer</button>
         </div>
       </form>
+    </div>
+
+    <!-- display help according to the user's OS -->
+    <div class="help-message">
+      Pour afficher ton adresse MAC:
+      <ul v-if="userOS == 'windows'">
+        <li>Presse les touches <b>Windows</b> et <b>R</b> en même temps</li>
+        <li>Tape <b>cmd</b> dans la barre de recherche</li>
+        <li>Execute <b>ipconfig /all</b></li>
+        <li>Ta MAC est l'<b>adresse physique</b> affichée</li>
+      </ul>
+      <ul v-else-if="userOS == 'mac'">
+        <li>Va dans <b>préférences système</b></li>
+        <li>Onglet <b>réseau</b></li>
+        <li>Clique sur <b>avancé</b></li>
+        <li>Ton adresse MAC est affichée !</li>
+      </ul>
+      <ul v-else>
+        <li>Ouvre un terminal</li>
+        <li>Execute <b>ifconfig -a</b></li>
+        <li>Ta MAC est la valeur qui suit <b>HWaddr</b></li>
+      </ul>
     </div>
   </div>
 </template>
@@ -29,6 +51,12 @@ export default {
   computed: {
     validateMacAddress: function() {
       return (/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/).test(this.macAddress)
+    },
+    userOS: function() {
+      // return the user OS
+      if (navigator.appVersion.indexOf("Win")!=-1) return "windows";
+      if (navigator.appVersion.indexOf("Mac")!=-1) return "mac";
+      return "linux";
     }
   },
   methods: {
--- src/components/SuccessfulConnection.vue
+++ src/components/SuccessfulConnection.vue
@@ -2,7 +2,7 @@
   <div>
     <h2 class="a-title a-title--blue center">Connexion réussie !</h2>
     <div class="center">
-      Débranche, puis rebranche ton cable éthernet pour être placés dans le réseau correspondant à ton tournois !
+      Débranche, puis rebranche ton cable éthernet pour être placé dans le réseau correspondant à ton tournois !
     </div>
   </div>
 </template>
